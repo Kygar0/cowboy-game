4101272706f3bd706dc6ed562c7ede8b1e94044f
Kygar0 <56590264+Kygar0@users.noreply.github.com>
2020-09-30 10:04:17 +0200

    Initial commit

---
+++ Assets/online_server.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class online_server : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("hey i'm conekted" + PhotonNetwork.CloudRegion);
+    }
+}
