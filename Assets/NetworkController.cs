2c3e71316ca498899d8748dd5e5b116e514c0f7d
Kygar0 <56590264+Kygar0@users.noreply.github.com>
2020-09-30 11:01:54 +0200

    online server v1

---
+++ Assets/NetworkController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    Button startbutton;
+    [SerializeField]
+
+
+    void Start()
+    {
+        startbutton.interactable = false;
+        PhotonNetwork.ConnectUsingSettings();
+        print("conecting...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("connekted to cerver in" + PhotonNetwork.CloudRegion);
+        startbutton.interactable = true;
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void shooting()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
