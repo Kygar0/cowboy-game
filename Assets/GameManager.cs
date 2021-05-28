46f483c95635c36663f1642e44e80705ca5af1f7
JSsu19 <60390385+JSsu19@users.noreply.github.com>
2021-05-28 23:01:15 +0200

    WubWub
    YEEEE

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Diagnostics;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject iron;
+    [SerializeField]
+    private AudioClip[] audioClips;
+
+    public GameObject arrowImage;
+    public GameObject goText;
+    public GameObject waitText;
+    public GameObject startButton;
+
+    private AudioSource audioSource;
+    private AudioClip currentClip;
+
+    bool drawnCheck = false;
+    bool startCheck = true;
+
+    int rand_num;
+    Stopwatch stopWatch = new Stopwatch();
+    Stopwatch stoopWatch = new Stopwatch();
     public static GameManager Instance;
     private void Awake()
     {
@@ -12,10 +34,56 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
+        audioSource = GetComponent<AudioSource>();
+        currentClip = audioClips[0];
+
+        startButton.GetComponent<Button>().onClick.AddListener(Startation);
+    }
+
+    private void Startation()
+    {
+        System.Random rd = new System.Random();
+
+        rand_num = rd.Next(1, 5);
+
+        print(rand_num + " sec");
+
+        stopWatch.Start();
+
+        startCheck = false;
+    }
+    private void Update()
+    {
+        if (startCheck == false)
+        {
+            startButton.SetActive(false);
+            waitText.SetActive(true);
+            TimeSpan ts = stopWatch.Elapsed;
+            int elapsedTime = (int)ts.TotalSeconds;
+
+            if (drawnCheck == false && elapsedTime >= rand_num)
+            {
+                drawnCheck = true;
+                startCheck = true;
+                elapsedTime = 0;
+                stopWatch.Stop();
+                SpriteRenderer sr = iron.GetComponent<SpriteRenderer>();
+                sr.enabled = true;
+                audioSource.PlayOneShot(currentClip);
+                stoopWatch.Start();
+                waitText.SetActive(false);
+                goText.SetActive(true);
+                arrowImage.SetActive(true);
+            }
+        }
     }
 
     public void Win()
     {
-        print("win");
+        TimeSpan ts = stoopWatch.Elapsed;
+        int elapsedTime = (int)ts.TotalMilliseconds;
+        print("RT: " + elapsedTime+" ms");
+        goText.SetActive(false);
+        arrowImage.SetActive(false);
     }
 }
---
+++ Assets/Highscores.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Highscores : MonoBehaviour
+{
+    const string privateCode = "-ey9Q1Rb30iYMlHyhCKFMQcvDG_k3YbUWOrQIoOBSkmw";
+    const string publicCode = "5fce89e6eb36fd27145cf4fd";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public event Action<Highscore[]> OnDownloadDone = delegate { };
+
+    public Highscore[] highscoreList;
+
+    public void AddNewHighscore(string username, int score)
+    {
+        StartCoroutine(UploadNewHighscore(username, score));
+    }
+
+    public IEnumerator UploadNewHighscore(string username, int score)
+    {
+        UnityWebRequest www = new UnityWebRequest(webURL + privateCode + "/add/" + UnityWebRequest.EscapeURL(username) + "/" + score);
+        www.SendWebRequest();
+        yield return www;
+
+        if (www.isNetworkError)
+        {
+            Debug.LogError("Error Uploading " + www.error);
+        }
+        else
+        {
+            print("Upload Successful");
+            DownloadHighscores();
+        }
+    }
+
+    public void DownloadHighscores()
+    {
+        StartCoroutine(DownloadHighscoresFromDatabase());
+    }
+
+    public IEnumerator DownloadHighscoresFromDatabase()
+    {
+        UnityWebRequest www = new UnityWebRequest(webURL + publicCode + "/pipe/");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError)
+        {
+            Debug.LogError("Error Uploading " + www.error);
+        }
+        else
+        {
+            FormatHighscores(www.downloadHandler.text);
+        }
+        OnDownloadDone(highscoreList);
+    }
+
+    private void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoreList = new Highscore[entries.Length];
+
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];
+            int score = int.Parse(entryInfo[1]);
+
+            highscoreList[i] = new Highscore(username, score);
+        }
+    }
+}
+
+public struct Highscore
+{
+    public string username;
+    public int score;
+
+    public Highscore(string name, int scorr)
+    {
+        username = name;
+        score = scorr;
+    }
+}
